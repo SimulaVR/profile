2e4a051f6b5fa0c0d3dd15f8a36708df9329f5db
George Singer <george.w.singer@gmail.com>
2021-05-01 20:26:46 -0500

    Set up basic reference project for haskell + nix profiling.

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ }:
+let
+  overlays = [
+      (self: super: {
+        haskellPackagesPIC = super.haskell.packages."ghc8104".override {
+          overrides = self: super: { ghc = super.ghc.override { enableRelocatedStaticLibs = true; enableProfiledLibs = true;}; };
+        };
+      })
+      (self: super: {
+        haskellPackagesPIC = super.haskellPackagesPIC.override (old: {
+          overrides = super.lib.composeExtensions (old.overrides or (_: _: {}))
+            (hself: hsuper: {
+              mkDerivation = x: (hsuper.mkDerivation x).override (old: {
+                enableLibraryProfiling = false;
+                doCheck = false;
+                configureFlags = (old.configureFlags or []) ++ ["--ghc-options=-fPIC -fexternal-dynamic-refs"];
+              });
+            });
+        });
+      })
+    ];
+    pkgs = import  ./pinned-nixpkgs.nix { overlays = overlays; };
+in
+  (pkgs.haskellPackagesPIC.callPackage) ./profile.nix { }
