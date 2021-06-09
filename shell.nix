368c3122e2750016be41ab37f9851dd8020f4570
George Singer <george.w.singer@gmail.com>
2021-06-08 23:21:34 -0500

    Fix shell.nix typos

--- shell.nix
+++ shell.nix
@@ -3,13 +3,13 @@ let
   overlays = (import ./profileOverlays.nix);
   pkgs = import  ./pinned-nixpkgs.nix { overlays = overlays; };
 
-  haskellCallPkg = if profileBuild then (pkgs.haskellPackagesPIC.callPackage) else (haskellPackages.callPackage);
+  haskellCallPkg = pkgs.haskellPackagesPIC.callPackage;
   haskellCallPkgNoProfile = pkgs.haskellPackages.callPackage;
   Cabal = haskellCallPkgNoProfile ./submodules/cabal/Cabal/Cabal.nix { };
-  hackage-security = haskellPackages.hackage-security.override { Cabal = Cabal; };
+  hackage-security = pkgs.haskellPackages.hackage-security.override { Cabal = Cabal; };
   cabal-install = haskellCallPkgNoProfile ./submodules/cabal/cabal-install/cabal-install.nix { Cabal = Cabal; hackage-security = hackage-security; };
 
-  main = (pkgs.haskellPackagesPIC.callPackage) ./Main.nix { cabal-install = cabal-install; };
+  main = haskellCallPkg ./Main.nix { cabal-install = cabal-install; };
 
   # ourCabal = stdenv.mkDerivation {
   #   name = "ourCabal";
@@ -21,4 +21,4 @@ let
   #   '';
   # };
 
-in main;
+in main
